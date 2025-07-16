47bd4a8b17174611d97b02b9e6272856fa4966f9
Sachi Jitendra Hansalia <sachih@meta.com>
2025-07-16 13:34:14 -0700

    fixed array issues when moving to last index

--- client/src/components/DragDropTest.jsx
+++ client/src/components/DragDropTest.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import '../styles/DragDropTest.css';
 const DAYS = 7;
 const DIVS = [
@@ -16,17 +16,21 @@ const DragDropTest = () => {
             .map(() => [])
     );
     const [droppedDivs, setDroppedDivs] = useState([]);
+    const dragOverItem = useRef();
+    const draggedItem = useRef({ day: null, index: null, name: null });
 
-    for (let i = 0; i < DAYS; i++) {
-        const day = `Day ${i + 1}`;
-    }
-
-    const handleDrag = (e, name) => {
+    const handleDrag = (e, name, dayIndex = null, index = null) => {
+        draggedItem.current = { day: dayIndex, index: index, name: name };
         e.dataTransfer.setData('name', name);
+        console.log(draggedItem.current);
     };
 
     const handleOnDragOver = (e) => {
         e.preventDefault();
+    };
+
+    const handleDragEnter = (e, index) => {
+        dragOverItem.current = index;
     };
 
     const handleDropOutside = (e) => {
@@ -35,17 +39,35 @@ const DragDropTest = () => {
         setDays((prev) => prev.map((day) => day.filter((n) => n !== name)));
     };
 
-    const handleDropOnDay = (e, index) => {
-        const name = e.dataTransfer.getData('name');
-        setDays((prev) =>
-            prev.map((day, i) =>
-                i === index
-                    ? [...day.filter((n) => n !== name), name]
-                    : day.filter((n) => n !== name)
-            )
-        );
-        setDroppedDivs((prev) => prev.filter((n) => n !== name));
+    const handleDropReorder = (e, dayIndex, onDayIndex = null) => {
+        onDayIndex = dragOverItem.current;
+        const { day, index, name } = draggedItem.current;
+        if (!name) return;
+        setDays((prev) => {
+            let newDays = prev.map((arr) => [...arr]);
+            if (day !== null) {
+                newDays[day].splice(index, 1);
+                newDays[dayIndex].splice(index, 0, name);
+            } else {
+                setDroppedDivs((d) => d.filter((n) => n !== name));
+            }
+            if (onDayIndex !== null && day === dayIndex) {
+                newDays[dayIndex].splice(index, 1);
+                newDays[dayIndex].splice(onDayIndex, 0, name);
+            } else {
+                if (onDayIndex !== null) {
+                    newDays[dayIndex].splice(onDayIndex, 0, name);
+                } else {
+                    newDays[dayIndex].push(name);
+                }
+            }
+
+            return newDays;
+        });
+
+        draggedItem.current = { day: null, index: null, name: null };
     };
+
     useEffect(() => {
         setDroppedDivs(DIVS);
     }, []);
@@ -67,7 +89,9 @@ const DragDropTest = () => {
                         {droppedDivs.map((div) => (
                             <div
                                 draggable
-                                onDragStart={(e) => handleDrag(e, div)}
+                                onDragStart={(e) =>
+                                    handleDrag(e, div, null, null)
+                                }
                             >
                                 {div}
                             </div>
@@ -75,18 +99,26 @@ const DragDropTest = () => {
                     </div>
                 </div>
                 <div className="days-cols">
-                    {days.map((dayItems, index) => (
+                    {days.map((dayItems, dayIndex) => (
                         <div
                             className="day-col"
                             onDragOver={handleOnDragOver}
-                            onDrop={(e) => handleDropOnDay(e, index)}
+                            onDrop={(e) => handleDropReorder(e, dayIndex)}
                         >
-                            <h2>Day {index + 1}</h2>
-                            {dayItems.map((div) => (
+                            <h2>Day {dayIndex + 1}</h2>
+                            {dayItems.map((div, index) => (
                                 <div
                                     className="draggable-div"
                                     draggable
-                                    onDragStart={(e) => handleDrag(e, div)}
+                                    onDragStart={(e) =>
+                                        handleDrag(e, div, dayIndex, index)
+                                    }
+                                    onDragEnter={(e) =>
+                                        handleDragEnter(e, index)
+                                    }
+                                    onDrop={(e) =>
+                                        handleDropReorder(e, dayIndex, index)
+                                    }
                                 >
                                     {div}
                                 </div>
